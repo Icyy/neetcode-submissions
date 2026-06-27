7d2bb66ba14565108c539325b28ea8ef8321e9a0
Icyy <30555447+Icyy@users.noreply.github.com>
2026-06-28 00:18:17 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const sorted = strs.map((str)=>str.split('').sort().join(''))
+        console.log(sorted)
+        const obj = {}
+        for(let i=0; i<sorted.length;i++){
+            if(!obj[sorted[i]]){
+                obj[sorted[i]] = [strs[i]]
+            }else{
+                obj[sorted[i]].push(strs[i])
+            }
+        }
+        return Object.values(obj)
+    }
+}
